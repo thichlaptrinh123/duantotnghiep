cf7f62a23df0f6f803d1a4fb05eec0db421c647a
thichlaptrinh123 <nkietnqps39401@gmail.com>
2025-03-22 15:20:24 +0700

    Ket noi database

--- Duantn/app.js
+++ Duantn/app.js
@@ -4,6 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const mongoose = require('mongoose');
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -18,6 +20,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+mongoose.connect('mongodb://localhost:27017/duantotnghiep')
+  .then(()=> console.log('connected to mongodb'))
+  .catch(err => console.error('fail to connect to mongodb' + err)) ;
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
